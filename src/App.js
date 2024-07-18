411fc97a1f0026bebf869844a36b860d0ffef04e
Hamza Azakaw <hamza.irfan@azakaw.com>
2024-07-18 18:00:03 +0500

    added new project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch } from 'react-router-dom';
-import AppRoute from './utils/AppRoute';
-import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
+import React, { Component } from 'react';
+import MusicJS from './js';
+import { injectGlobal } from 'styled-components';
 
-// Layouts
-import LayoutDefault from './layouts/LayoutDefault';
+injectGlobal`
+   body {
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+      -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+`;
 
-// Views 
-import Home from './views/Home';
-
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
-
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
-
-const App = () => {
-
-  const childRef = useRef();
-  let location = useLocation();
-
-  useEffect(() => {
-    const page = location.pathname;
-    document.body.classList.add('is-loaded')
-    childRef.current.init();
-    trackPage(page);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
-
-  return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-        <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-        </Switch>
-      )} />
-  );
+class App extends Component {
+   render() {
+      return (
+         <div className="App">
+            <MusicJS />
+         </div>
+      );
+   }
 }
+
+document.addEventListener('touchstart', function() {}, true);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-//import './App.css';
-import './assets/scss/style.scss';
-
-const history = createBrowserHistory();
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './js/rootReducer';
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
